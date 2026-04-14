268c87e47df0dff7ee51672c7a936cb829cd863d
Bruno Sabença <bruno@brunosabenca.com>
2026-04-14 21:38:50 +0100

    fix: split envoy-gateway kustomization and adapt repo for brunosabenca
    - Replace all nce.wtf references with brunosabenca.com
    - Update region to uk-london-1, namespace to lray0fqv9epr
    - Update k8s version to v1.34.1, ssh key, github org
    - Remove NSG annotations (Quick Create uses security lists)
    - Split envoy-gateway into helm + config kustomizations with dependsOn
      to resolve Gateway API CRD bootstrap ordering
    - Remove home DNS records file
    - Remove dns-records kustomization (no records defined)
    - Fix certificate/secret names from wildcard-nce-wtf to wildcard-brunosabenca-com

--- terraform/infra/_variables.tf
+++ terraform/infra/_variables.tf
@@ -7,14 +7,14 @@ variable "region" {
   description = "OCI region"
   type        = string
 
-  default = "eu-frankfurt-1"
+  default = "uk-london-1"
 }
 
 variable "ssh_public_key" {
   description = "SSH Public Key used to access all instances"
   type        = string
 
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBPjsPbPbEsCywcFfS24iBUV1ISMM+5Yk0eqWuaNSP8YqjgPkJU5K62Pm8tRYUpfoP2mkF5zdT3Zj+6kMtqxkACcvQDui71PzIVQx57AE4wcvsEYXqLYNpvHl/YEdf7fCNvsXounnJjYSHbjRPjTcq+34CgedCVFL5MYXpdRmc5Kl1Do8JscYm5AzVOhfRJJ0Fiqd4bkRMpJN5zYZ+NYw/cnSKFckSTsG4pSbcSCoR1wPNRU6rEPXSQa2hFZPpYORuxKcwua/bb3aRzyU1fT7xdjzkDs++0rQJQ461kvBjsYgD5Zuwgl3MkzouVx2p5ic1dU34kQTrWpH3z5diRut7 ull@rsa"
+  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHsCy4YZhtQxG9K1fe1Lp2760ERU3k8HHWviFB8/jhTv bruno@monolith"
 }
 
 variable "kubernetes_version" {
@@ -22,7 +22,7 @@ variable "kubernetes_version" {
   description = "Version of Kubernetes"
   type        = string
 
-  default = "v1.33.1"
+  default = "v1.34.1"
 }
 
 variable "kubernetes_worker_nodes" {
